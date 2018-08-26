7a34b868b87b978e010b3567b67423695a36e974
ThunderBirdsX3 <thunder_birds_no.xiii@live.com>
2018-08-26 19:40:03 +0700

    Add sweetalert

--- exercise/resources/assets/js/app.js
+++ exercise/resources/assets/js/app.js
@@ -9,6 +9,8 @@ window.Popper = require('popper.js').default;
 try {
     window.$ = window.jQuery = require('jquery');
 
+    window.swal = require('sweetalert2');
+
     require('bootstrap');
 } catch (e) {}
 
